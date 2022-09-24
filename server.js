b14fed80b9c009125b341031028ce30a2add6e13
kolbykimball <kolby.kimball10@gmail.com>
2022-09-24 11:32:37 -0600

    updates - cant get npm run start to work

--- server.js
+++ server.js
@@ -25,8 +25,6 @@ const startPrompt = () => {
                 'View All Employees',
                 'View All Roles',
                 'View All Departments',
-                'Update Employee Role',
-                'Update Employee Manager',
                 'Add Employee',
                 'Add Role',
                 'Add Department',
@@ -47,14 +45,6 @@ const startPrompt = () => {
 
             if (choices === 'Add Employee') {
                 addEmployee();
-            }
-
-            if (choices === 'Update Employee Role') {
-                updateEmployeeRole();
-            }
-
-            if (choices === 'Update Employee Manager') {
-                updateEmployeeManager();
             }
 
             if (choices === 'View All Roles') {
@@ -106,4 +96,140 @@ viewEmployees = () => {
         console.table(rows);
         mainMenu();
     });
+};
+
+addEmployee = () => {
+    connection.query('SELECT * FROM role', function (err, res) {
+        if (err) throw err;
+        inquirer
+            .prompt([
+                {
+                    name: 'first_name',
+                    type: 'input',
+                    message: "What is the employee's first name?"
+                },
+                {
+                    name: 'last_name',
+                    type: 'input',
+                    message: "What is the employee's last name?"
+                },
+                {
+                    name: 'manager_id',
+                    type: 'input',
+                    message: "What is the employee's manager's ID?"
+                },
+                {
+                    name: 'role',
+                    type: 'list',
+                    choices: function() {
+                        var roleArray = [];
+                        for (let i = 0; i < res.length; i++) {
+                            roleArray.push(res[i].title);
+                        }
+                        return roleArray;
+                    },
+                    message: "What is this employee's role?"
+                }
+            ]).then(function (answer) {
+                let role_id;
+                for (let a = 0; a < res.length; a++) {
+                    if (res[a].title == answer.role) {
+                        role_id = res[a].id;
+                        console.log(role_id)
+                    }
+                }
+                connection.query(
+                    'INSERT INTO employee SET ?',
+                    {
+                        first_name: answer.first_name,
+                        last_name: answer.last_name,
+                        manager_id: answer.manager_id,
+                        role_id: role_id,
+                    },
+                    function (err) {
+                        if (err) throw err;
+                        console.log('Employee has been added!');
+                        mainMenu();
+                    })
+            })
+    })
+};
+
+ const addDepartment = () => {
+    inquirer
+        .prompt([
+            {
+                name: 'newDepartment', 
+                type: 'input', 
+                message: 'Which department would you like to add?'
+            }
+            ]).then(function (answer) {
+                connection.query(
+                    'INSERT INTO department SET ?',
+                    {
+                        name: answer.newDepartment
+                    });
+                var query = 'SELECT * FROM department';
+                connection.query(query, function(err, res) {
+                if(err)throw err;
+                console.log('Department has been added!');
+                console.table('All Departments:', res);
+                mainMenu();
+                })
+            })
+};
+
+addRole = () => {
+    connection.query('SELECT * FROM department', function(err, res) {
+        if (err) throw err;
+        inquirer
+        .prompt([
+            {
+                name: 'new_role',
+                type: 'input',
+                message: "What new role would you like to add?"
+            },
+            {
+                name: 'salary',
+                type: 'input',
+                message: "What is the salary of this role?"
+            },
+            {
+                name: 'Department',
+                type: 'list',
+                choices: function() {
+                    var deptArray = [];
+                    for (let i =0; i <res.length; i++) {
+                        deptArray.push(res[i].name);
+                    }
+                    return deptArray;
+                },
+            }
+        ]).then(function (answer) {
+            let department_id;
+            for (let a = 0; a < res.length; a++) {
+                if (res[a].name == answer.Department) {
+                    department_id = res[a].id;
+                }
+            }
+            connection.query(
+                'INSERT INTO role SET?',
+                {
+                    title: answer.new_role,
+                    salary: answer.salary,
+                    department_id: department_id
+                },
+                function (err, res) {
+                    if(err) throw err;
+                    console.log('New role has been added.');
+                    var query = 'SELECT * FROM role';
+                    connection.query(query, function(err, res) {
+                    if(err)throw err;
+                    console.table('All Roles:', res);
+                    mainMenu();
+                    })
+                  
+                })
+        })
+    })
 };
