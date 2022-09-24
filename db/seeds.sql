2bc0b43f24b488742dbd01f0b5ac2fd44c871c80
kolbykimball <kolby.kimball10@gmail.com>
2022-09-24 11:11:25 -0600

    sql files created, some serverjs created

---
+++ db/seeds.sql
@@ -0,0 +1,21 @@
+INSERT INTO department(department_name)
+VALUES
+("Executives"), 
+("Management"), 
+("Sales"), 
+("Fetch");
+
+INSERT INTO role(title, salary, department_id)
+VALUES
+("CEO", 150000, 1), 
+("Project Manager", 125000, 2), 
+("Sales Lead", 100000, 3), 
+("Lead Fetcher", 50000, 4);
+
+INSERT INTO employee(id, first_name, last_name, role_id, manager_id)
+VALUES 
+(1, 'Kolby', 'Kimball', 1, 1), 
+(2, 'Janessa', 'Kimball', 2, 2), 
+(3, 'Nala', 'Bear', 3, 3), 
+(4, 'Indy', 'Girl', 4, 4), 
+
--- server.js
+++ server.js
@@ -75,3 +75,35 @@ const startPrompt = () => {
         });
 };
 
+viewDepartments = () => {
+    console.log('Showing all departments.../n');
+    const sql = 'SELECT department.id AS id, department.name AS department FROM department';
+
+    connection.query(sql, (err, rows) => {
+        if (err) throw err;
+        console.table(rows);
+        mainMenu();
+    });
+};
+
+viewRoles = () => {
+    console.log('Showing all roles.../n');
+    const sql = 'SELECT role.id, role.title, department.name AS department FROM role INNER JOIN department ON role.department_id = department.id';
+
+    connection.query(sql, (err, rows) => {
+        if (err) throw err;
+        console.table(rows);
+        mainMenu();
+    });
+};
+
+viewEmployees = () => {
+    console.log('Showing all employees.../n');
+    const sql = 'SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id';
+
+    connection.query(sql, (err, rows) => {
+        if (err) throw err;
+        console.table(rows);
+        mainMenu();
+    });
+};
